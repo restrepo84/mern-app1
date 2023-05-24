9cad6d0d617b42fcd0b57db203ce515e3d09fab0
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-05-24 02:34:12 -0600

    react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch
-} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
+import Users from "./user/pages/Users";
+// import NewPlace from './places/pages/NewPlace';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <Route path="/">
+        <Users />
+      </Route>
     </Router>
   );
 };
