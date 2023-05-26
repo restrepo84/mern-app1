a4cf4e32db0e812cc5bcc90b5f87d1ad1c04dd17
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-05-26 13:52:27 -0600

    hooks and validation

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import UserItem from "./UserItem";
-import Card from "../../shared/components/UIElements/Card";
-import "./UsersList.css";
+import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
 
-const UsersList = (props) => {
+const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
         <Card>
-          <h2>No user found.</h2>
+          <h2>No users found.</h2>
         </Card>
       </div>
     );
@@ -17,7 +17,7 @@ const UsersList = (props) => {
 
   return (
     <ul className="users-list">
-      {props.items.map((user) => (
+      {props.items.map(user => (
         <UserItem
           key={user.id}
           id={user.id}
